448bce6dbc5459c5d02cfcfc749655d34fb761c3
SichangiFelix <sichangifelix23@gmail.com>
2023-01-07 15:31:45 +0300

    Built profile screen

---
+++ lib/pages/profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:social_network/widgets/progress.dart';
+
+import '../widgets/header.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: header(context, titleText: 'Profile'),
+      body: circularProgress(),
+    );
+  }
+}
