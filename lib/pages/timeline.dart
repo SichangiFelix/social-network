b9b89dc8bc872dce3d8cb4b08e9c1164d682cd88
SichangiFelix <sichangifelix23@gmail.com>
2023-01-07 15:31:58 +0300

    Built profile screen

---
+++ lib/pages/timeline.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../widgets/header.dart';
+
+class Timeline extends StatefulWidget {
+  const Timeline({super.key});
+
+  @override
+  State<Timeline> createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: header(context, isAppTitle: true),
+      body: Text('Timeline'),
+    );
+  }
+}
