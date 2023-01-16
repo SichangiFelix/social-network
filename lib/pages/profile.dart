03f2cf3793f43f8ddabbc3d5542befc4b832c53c
SichangiFelix <sichangifelix23@gmail.com>
2023-01-16 10:57:08 +0300

    Build the header section of the profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,22 +1,186 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:social_network/pages/home.dart';
 import 'package:social_network/widgets/progress.dart';
+import 'package:social_network/pages/edit_profile.dart';
 
+import '../models/user.dart';
 import '../widgets/header.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Profile extends StatefulWidget {
-  const Profile({super.key});
+  final String? profileId;
+  const Profile({super.key, this.profileId});
 
   @override
   State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser!.id;
+
+  buildCountColumn(String label, int count) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          count.toString(),
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4),
+          child: Text(
+            label,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildProfileButton() {
+    //If viewing our own profile display edit profile button
+    bool isProfileOwner = currentUserId == widget.profileId!;
+    if(isProfileOwner){
+       return buildButton(text: 'Edit Profile', function: editProfile);
+    }
+  }
+
+  editProfile(){
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return EditProfile(currentUserId: currentUserId);
+    }));
+  }
+
+  buildButton({required String text, required VoidCallback function}){
+    return Container(
+      padding: EdgeInsets.only(top: 2),
+      child: TextButton(
+        onPressed: function,
+        child: Container(
+          width: 250,
+          height: 27,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              color: Colors.blue,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildProfileHeader() {
+    return FutureBuilder(
+      future: usersRef.doc(widget.profileId).get(),
+      builder: (context, snapshot) {
+        if (snapshot.data == null) {
+          circularProgress();
+        }
+        User user = User.fromDocument(snapshot.data!);
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildCountColumn("posts", 0),
+                            buildCountColumn("followers", 0),
+                            buildCountColumn("following", 0),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildProfileButton(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(
+                  top: 12,
+                ),
+                child: Text(
+                  user.username == null? "null" : user.username!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(
+                  top: 4,
+                ),
+                child: Text(
+                  user.displayName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(
+                  top: 2,
+                ),
+                child: Text(
+                  user.bio,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: 'Profile', implyLeading: true),
-      body: circularProgress(),
+      body: ListView(
+        children: [
+          buildProfileHeader(),
+        ],
+      ),
     );
   }
 }
